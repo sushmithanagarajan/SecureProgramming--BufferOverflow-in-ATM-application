8c110be823f6d5ec1242b0a54f6baf90ae049e3e
Sushmitha Nagarajan <38819267+sushmithanagarajan@users.noreply.github.com>
2019-02-19 02:40:39 -0600

    Add files via upload

--- ATMBufferOverflow.c
+++ ATMBufferOverflow.c
@@ -1,73 +1,63 @@
 #include<stdio.h>
+#include<conio.h>
 #include<string.h>
 int authentication(void);
 int deposit();
 int withdraw();
 void checkbalance(int balance);
 
-int balance=0;
-int options=0;
+int balance=300;
+int options;
 int receipt;
 
-int authentication()
+int main(void)
 {
-    int secret=0;
     char pipe[5];
+    int secret;
     printf("\n Enter the password \n");
     gets(pipe);
     if((strcmp(pipe,"1234"))==0)
     {
-        printf("\n Correct Password \n");
+        printf("\n Corect Password");
         secret=1;
     }
     else
     {
-        printf("\n Wrong Password \n");
-    }
-return secret;
-
-}
-
-int main(void)
-{
-    int status=authentication();
-    if (status)
-    {
+        printf("wrong");
         
-       printf("\n Root Access given to the user \n");
-	 while(options<=4){
-	printf("\n ****************ATM Transactions************** \n");
-        printf("\n Choose from the following options \n 1.Deposit \n 2.Withdraw \n 3.Check Balance\n 4.Exit");
+    }
+    if (secret)
+    {
+        printf("\n Root access is given to the user");
+        printf("Choose from the following options \n 1.Deposit \n 2.Withdraw \n 3.Check Balance");
         scanf("%d", &options);
+        while(options<=3)
+        {
         switch(options)
         {
             case 1:
-            balance=deposit();
+            deposit();
             break;
             
             case 2:
-            balance=withdraw();
+            withdraw(balance);
             break;
             
             case 3:
             checkbalance(balance);
             break;
-		
-	    case 4: exit(0);
         }
         }
-    
-        printf("\n Would you like to print a receipt\n");
+        printf("Would you like to print a receipt\n");
         scanf("%d", &receipt);
     }
-return 0;
     
 }
 
 int deposit()
 {
     int depositAmount;
-    printf("\n Enter the amount you like to deposit\n");
+    printf("Enter the amount you like to deposit");
     scanf("%d", &depositAmount);
     balance += depositAmount;
     return balance;
@@ -76,7 +66,7 @@ int deposit()
 int withdraw()
 {
     int withdrawAmount;
-    printf("\n Enter the amount to withdraw \n");
+    printf("Enter the amount to withdraw");
     scanf("%d", &withdrawAmount);
     balance -= withdrawAmount;
     return balance;
@@ -84,6 +74,6 @@ int withdraw()
 
 void checkbalance(int balance)
 {
-    printf("\n The available balance is \n %d", balance);
+    printf("The available balance is \n", &balance);
     
 }
